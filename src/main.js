2b8a3b9565084f333e8677d63d79c183c8828200
csherman2828 <csherman2828@gmail.com>
2020-08-18 00:20:41 -0500

    add vuex state management

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './app'
 import vuetify from './plugins/vuetify';
 import router from './plugins/router';
+import store from './plugins/vuex';
 
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
